08fe470191e98db131eb79b05eb1d2bde7f2911c
Lucas Morais <lucascdemorais@outlook.com>
2019-12-07 10:26:22 -0300

    Removing Navigation 5.0

--- App.js
+++ App.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import { NavigationNativeContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import ImageContainer from 'src/container/imageContainer';
 import SettingsContainer from 'src/container/settingsContainer';
 
-const Drawer = createDrawerNavigator();
+const App = createDrawerNavigator({
+  Dashboard:{
+    screen: ImageContainer
+  },
+  Settings:{
+    screen:SettingsContainer
+  }
 
-const App = () => {
-  return (
-    <NavigationNativeContainer>
-      <Drawer.Navigator >
-        <Drawer.Screen
-          name={'Painel'}
-          component={ImageContainer}
-          options={{
-            title:'Painel',
-          }}
-        />
-        <Drawer.Screen
-          name={'Opções'}
-          component={SettingsContainer}
-          options={{
-            title: 'Opções'
-          }}
-        />
-      </Drawer.Navigator>
-    </NavigationNativeContainer>
-  );
-};
+})
 
-export default App;
+export default createAppContainer(App);
